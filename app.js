ef050dd4f679e0d86c4f4d28287b03fb74216322
Dimitrios Tikas <tikasdimitrios@gmail.com>
2020-12-08 20:20:40 +0100

    added static served images

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ const bodyParser = require('body-parser');
 const corsPolicy = require('./config/corsPolicy');
 const mongoose = require('mongoose');
 const myError = require('./controllers/error');
+const path = require('path');
 
 const app = express();
 
+//express customization
 app.use(bodyParser.json());
 app.use(corsPolicy.setCorsHeaders);
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+//routes
 app.use('/game', gameRoutes);
 app.use(myError.handleErrors);
 
